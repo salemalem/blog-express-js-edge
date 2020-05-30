98c4e792810cd73556c4f248b073881a942c3032
salemalem <wymedia@protonmail.com>
2020-05-30 20:58:03 +0600

    Add Post Page, Edge Templater added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
+const mongoose = require('mongoose');
  
 const app = new express();
+
+mongoose.connect('mongodb://localhost:27017/mydb', { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err));
  
 app.use(express.static('public'));
 app.use(expressEdge.engine);
@@ -10,6 +18,10 @@ app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
 });
  
 app.get('/about', (req, res) => {
