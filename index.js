04effc9931f0cb3ec689d611e27328022cb0e721
salemalem <wymedia@protonmail.com>
2020-05-31 18:34:52 +0600

    sessions are now stored in db

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require("express-fileupload");
 const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
 
 const createTutorialController = require('./controllers/createTutorial')
 const homePageController = require('./controllers/homePage')
@@ -19,10 +20,6 @@ const Tutorials = require('./database/models/Tutorials');
  
 const app = new express();
 
-app.use(expressSession({
-    secret: 'secret'
-}));
-
 mongoose.connect('mongodb://localhost:27017/mydb', { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -31,6 +28,17 @@ mongoose.connect('mongodb://localhost:27017/mydb', {
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err));
  
+const mongoStore = connectMongo(expressSession);
+
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    }),
+    resave: true,
+    saveUninitialized: true,
+}));
+
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge.engine);
@@ -43,10 +51,12 @@ app.use(bodyParser.urlencoded({
 const storeTutorial = require('./middleware/storeTutorial')
 app.use('/tutorials/store', storeTutorial)
 
+const auth = require("./middleware/auth");
+
 app.get(["/", "/tutorials"], 
     homePageController
 );
-app.get("/tutorials/new", createTutorialController);
+app.get("/tutorials/new", auth, createTutorialController);
 app.post("/tutorials/store", storeTutorialController);
 app.get("/tutorials/:id", getTutorialController);
 app.get('/login', loginController);
