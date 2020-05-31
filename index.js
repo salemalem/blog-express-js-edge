3cbdb5177d0f27ab1ca54b149be97b2e07df6fce
salemalem <wymedia@protonmail.com>
2020-05-31 19:31:08 +0600

    display login register links to guests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const expressEdge = require('express-edge');
 const express = require('express');
+const edge = require("edge.js");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require("express-fileupload");
@@ -44,6 +45,12 @@ app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge.engine);
 app.set('views', __dirname + '/views');
+
+app.use('*', (req, res, next) => {
+    edge.global('auth', req.session.userId)
+    next()
+});
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
